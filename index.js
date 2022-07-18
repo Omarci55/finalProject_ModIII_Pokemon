f3045ec94ca76f40384b2ca529445da3b86fb3ca
o.marci@gmx.de <o.marci@gmx.de>
2022-07-18 09:35:15 +0200

    debug

--- index.js
+++ index.js
@@ -23,27 +23,40 @@ class Pokemon {
         this.attacks.push( newAttack )
     } 
 
+    attack( attackToBeUsed, opponent ) {
+        
+            opponent.health -= this.attacks[attackToBeUsed].power;
 
-    attack( attackToBeUsed, competitor ) {
+            console.log(opponent.health)
+            this.magic -= this.attacks[attackToBeUsed].magic;
 
-        //the competitor loses health
-        competitor.health -= this.attacks[attackToBeUsed].power;
-
-        this.magic -= this.attacks[attackToBeUsed].magic
-
-        console.log(`${this.name} launched skill ${this.attacks[attackToBeUsed].skill} successfully! \n${competitor.name} got ${this.attacks[attackToBeUsed].power} of his health damage`)
+        console.log(`---- POKEMON ATTACK \n${this.name} launched skill ${this.attacks[attackToBeUsed].skill} successfully! \n${opponent.name} got ${this.attacks[attackToBeUsed].power} of his health damage`)
+       
     }
 
     //When logged to the console, this method add values to the properties
-    healCenter() {
-        this.health += 30
+    getMagic() {
+        //this.health += 30
         this.magic += 20
     }
 
     showStatus( ) {
-        console.log(`${this.name} has ${this.health} health and ${this.magic} magic level left.`)
+        
+        if ( this.name === "pikachu" && this.health < 30 ||
+            this.name === "bulbassaur" && this.health < 20 ||
+            this.name === "charmander" && this.health < 25) {
+
+            console.log( `${this.name} has not enough magic, cannot launch attack!` )
+
+        } else {
+
+            console.log( `---- STATUS ${this.name} \nHealth:${this.health} \nMagic: ${this.magic} ` )
+
+        }
+        
     }
 }
+
 
 //This method allows to create instances of new Attacks, which are gonna be add to the array Pokemon.attacks 
 class AttackSkill {
@@ -51,15 +64,13 @@ class AttackSkill {
     //It takes 3 param, which are gonna interact with the Pokemon properties (the first created class)
     constructor( skill, power, magic ) {
         
-        //Name of the skill
         this.skill = skill
-        //How much health takes from the competitor
         this.power = power
-        //how much of magic requires of himself for the attack
         this.magic = magic
     }
 
 } 
+
 //New instances Pokemons
 //The Pokemon pikachu starts the game with 120 Health and 80 Magic...
 const pikachu = new Pokemon( "pikachu", 120, 80 )
@@ -67,22 +78,38 @@ const bulbassaur = new Pokemon( "bulbassaur", 95, 105 )
 const charmander = new Pokemon( "charmander", 110, 85 )
 
 //New instances for AttackSkill
-//The Lightning(z. B.) Attack takes 40 of the competitor Health, but also 30 of magic of who launches the attack
+//The Lightning(z. B.) Attack takes 40 of the opponent Health, but also 30 of magic of who launches the attack
 const lightning = new AttackSkill( "lightning", 40, 30 )
 const poisonSeed = new AttackSkill ( "poison seed", 20, 20 )
 const flamethrower = new AttackSkill( "flamethrower", 35, 25)
 
-bulbassaur.learnAttackSkill( poisonSeed )
+
 pikachu.learnAttackSkill( lightning )
-
 console.log( pikachu )
 
-console.log( pikachu.attack(0, bulbassaur ) )
+bulbassaur.learnAttackSkill( poisonSeed )
 console.log( bulbassaur )
-console.log( pikachu )
-charmander.learnAttackSkill( flamethrower )
 
-pikachu.attack(0, bulbassaur);
+charmander.learnAttackSkill( flamethrower )
+console.log( charmander )
+
+pikachu.attack( 0, bulbassaur )
+
+/* pikachu.showStatus()
+pikachu.attack(0, bulbassaur ) 
+pikachu.learnAttackSkill( lightning )
+console.log( bulbassaur.attack(0, pikachu ) )
+pikachu.attack(0, bulbasaur);
+bulbasaur.attack(0, pikachu);
+pikachu.showStatus();
+bulbasaur.showStatus();
+pikachu.attack(0, bulbasaur);
+pikachu.attack(0, bulbasaur);
+pikachu.getMagic();
+pikachu.attack(0, bulbasaur);
+bulbasaur.attack(0, pikachu); */
+
+
 
 
 
